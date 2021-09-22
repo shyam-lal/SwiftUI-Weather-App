31115985a8dda573d1504cffed044e2d86378a69
shyamlal <shyamlal@qburst.com>
2021-09-22 17:52:27 +0530

    Create project and add background gradient

---
+++ weather-app-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather-app-test
+//
+//  Created by Qburst on 29/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]),
+                           startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
