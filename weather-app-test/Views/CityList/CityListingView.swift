48e3f6a52d8970a66aded08697198f47dc12220e
shyamlal <shyamlal@qburst.com>
2021-12-27 19:06:23 +0530

    Add file for row

---
+++ weather-app-test/Views/CityList/CityListingView.swift
@@ -0,0 +1,20 @@
+//
+//  CityListingView.swift
+//  weather-app-test
+//
+//  Created by shyamlal on 27/12/21.
+//
+
+import SwiftUI
+
+struct CityListingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityListingView_Previews: PreviewProvider {
+    static var previews: some View {
+        CityListingView()
+    }
+}
