42faff582d3a0744fd93d8d2bc8c1df144272f04
shyamlal <shyamlal@qburst.com>
2021-12-29 15:38:15 +0530

    Created city model

--- weather-app-test/Models/Cities.swift
+++ weather-app-test/Models/Cities.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+
+struct City: Identifiable {
+    var id: Int
+    var name: String
+}
+
+let cities = [
+    City(id: 1, name: "London"),
+    City(id: 2, name: "Paris"),
+    City(id: 3, name: "New York"),
+    City(id: 4, name: "Tokyo"),
+    City(id: 5, name: "Jakartha"),
+    City(id: 6, name: "Delhi"),
+    City(id: 1, name: "Manila"),
+    City(id: 2, name: "SHanghai"),
+    City(id: 3, name: "Karachi"),
+    City(id: 1, name: "Beijng"),
+    City(id: 2, name: "Sao Polo")
+]
