7d53d6823142144ecd517dd2d62744fba09a8d63
shyamlal <shyamlal@qburst.com>
2021-12-30 18:15:55 +0530

    Converted struct City to class and added init function

--- weather-app-test/Models/Cities.swift
+++ weather-app-test/Models/Cities.swift
@@ -8,21 +8,27 @@
 import Foundation
 
 
-struct City: Identifiable {
+class City: Identifiable, ObservableObject {
     var id: Int
     var name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
 }
 
 let cities = [
-    City(id: 1, name: "London"),
+    City(id: 1, name: "Mumbai"),
     City(id: 2, name: "Paris"),
     City(id: 3, name: "New York"),
     City(id: 4, name: "Tokyo"),
     City(id: 5, name: "Jakartha"),
     City(id: 6, name: "Delhi"),
-    City(id: 1, name: "Manila"),
-    City(id: 2, name: "SHanghai"),
-    City(id: 3, name: "Karachi"),
-    City(id: 1, name: "Beijng"),
-    City(id: 2, name: "Sao Polo")
+    City(id: 7, name: "Manila"),
+    City(id: 8, name: "SHanghai"),
+    City(id: 9, name: "Karachi"),
+    City(id: 10, name: "Beijng"),
+    City(id: 11, name: "Sao Polo"),
+    City(id: 12, name: "London")
 ]
