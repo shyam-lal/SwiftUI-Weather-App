21cdec48df971a945ec869a5b7c077644444fdf6
shyamlal <shyamlal@qburst.com>
2022-01-05 20:49:27 +0530

    Created model struct for forecast data

--- weather-app-test/Models/ForecastModel.swift
+++ weather-app-test/Models/ForecastModel.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct ForecastModel: Codable {
+    var forecast: ForecastData?
+}
+
+//struct LocationResponse: Codable {
+//    var name: String?
+//    var localtime: String?
+//}
+//
+//struct CurrentDetails: Codable {
+//    var temp_c: Float?
+//    var is_day: Int?
+//    var condition: ConditionDetails?
+//    var wind_kph: Float?
+//    var humidity: Int?
+//}
+
+struct ForecastData: Codable {
+    var forecastday: ForecastDay?
+}
+
+struct ForecastDay: Codable {
+    var forecastDataSource: [ForecastDataSource]
+}
+
+struct ForecastDataSource: Codable {
+    var date: String
+    var day: WeatherData
+}
+
+struct WeatherData: Codable {
+    var avgtemp_c: String
+    var condition: ConditionModel
+}
+
+struct ConditionModel: Codable {
+    var text: String
+}
