e3720eb25937eb01ba236627cc3a9aaec544e5ed
shyamlal <shyamlal@qburst.com>
2022-01-05 20:48:38 +0530

    Implement API call to get forecast data

--- weather-app-test/Managers/WeatherManager.swift
+++ weather-app-test/Managers/WeatherManager.swift
@@ -11,25 +11,23 @@ class WeatherManager {
     
     static var didFetchData: (() -> Void)?
     static var weatherData: CurrentWeather?
+//    static var forecastData
+    
     static func getCurrentWeatherDetails(city: String) {
-        
-        print(city)
-        print("-------------------")
         // Create URL
-        var urlComponents = URLComponents(string: "https://api.weatherapi.com/v1/current.json?key=8d5ce6ac991441f89db123830211312")
+        var urlComponents = URLComponents(string: "https://api.weatherapi.com/v1/current.json")
         urlComponents?.queryItems = [
             URLQueryItem(name: "key", value: "8d5ce6ac991441f89db123830211312"),
-            URLQueryItem(name: "q", value: "Mumbai")
+            URLQueryItem(name: "q", value: city)
         ]
         let url = urlComponents?.url
-        print(url)
 //        let url = URL(string: "https://api.weatherapi.com/v1/current.json?key=8d5ce6ac991441f89db123830211312&q=London")
 //        let url = URL(string: "https://606359fe0133350017fd318b.mockapi.io/api/v1/dummy")
         var request = URLRequest(url: url!)
         request.httpMethod = "GET"
 
         // Create Data Task
-        let task = URLSession.shared.dataTask(with: url!) { data, response, error in
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
             
             if let data = data {
                 if let weather = try? JSONDecoder().decode(CurrentWeather.self, from: data) {
@@ -41,6 +39,33 @@ class WeatherManager {
                 }
             } else if let error = error {
                 print("HTTP Request Failed \(error)")
+            }
+        }
+        task.resume()
+    }
+    
+    
+    static func getWeatherForecast(city: String) {
+        var urlComponents = URLComponents(string: "http://api.weatherapi.com/v1/forecast.json")
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "key", value: ""),
+            URLQueryItem(name: "q", value: city),
+            URLQueryItem(name: "days", value: "3"),
+            URLQueryItem(name: "aqi", value: "no")
+        ]
+        let url = urlComponents?.url
+        var request = URLRequest(url: url!)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let weather = try? JSONDecoder().decode(CurrentWeather.self, from: data) {
+                    print(weather)
+                } else {
+                    print("Invalid response")
+                }
+            } else if let error = error {
+                print("HTTP request Failed \(error)")
             }
         }
         task.resume()
