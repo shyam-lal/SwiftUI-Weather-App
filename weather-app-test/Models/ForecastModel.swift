f13f500fe55873ebddf3d6deb63c689b85a06a53
shyamlal <shyamlal@qburst.com>
2022-01-10 13:37:42 +0530

    Fixed forecast model

--- weather-app-test/Models/ForecastModel.swift
+++ weather-app-test/Models/ForecastModel.swift
@@ -11,34 +11,17 @@ struct ForecastModel: Codable {
     var forecast: ForecastData?
 }
 
-//struct LocationResponse: Codable {
-//    var name: String?
-//    var localtime: String?
-//}
-//
-//struct CurrentDetails: Codable {
-//    var temp_c: Float?
-//    var is_day: Int?
-//    var condition: ConditionDetails?
-//    var wind_kph: Float?
-//    var humidity: Int?
-//}
-
 struct ForecastData: Codable {
-    var forecastday: ForecastDay?
+    var forecastday: [ForecastDay]?
 }
 
 struct ForecastDay: Codable {
-    var forecastDataSource: [ForecastDataSource]
-}
-
-struct ForecastDataSource: Codable {
-    var date: String
-    var day: WeatherData
+    var date: String?
+    var day: WeatherData?
 }
 
 struct WeatherData: Codable {
-    var avgtemp_c: String
+    var avgtemp_c: Float?
     var condition: ConditionModel
 }
 
