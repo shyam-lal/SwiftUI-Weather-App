680e8dfeb0df9ddb884ada19fb17c14d2abaca43
shyamlal <shyamlal@qburst.com>
2022-01-10 18:03:45 +0530

    Moved daily weather view to seperate file

--- weather-app-test/Views/ContentView.swift
+++ weather-app-test/Views/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPhone 11")
+            .previewDevice("iPhone 7")
     }
 }
 
@@ -91,34 +91,7 @@ struct ContentView: View {
     }
 }
 
-struct DailyWeatherView: View {
-    var forecastDataSource: ForecastDay?
-    @Binding var isNight: Bool
-    var body: some View {
-        VStack {
-            Text(((forecastDataSource?.date?.stringToDate().dayOfWeek()) ?? "UHU").prefix(3))
-                .font(.system(size: 20, weight: .light))
-                .foregroundColor(isNight ? Color.white : Color.black)
-            
-            Image(systemName: ImageNames.cloudSunAndRain.rawValue)
-                .renderingMode(.original)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 50, height: 50)
-            
-            Text("\(String(Int(forecastDataSource?.day?.avgtemp_c ?? 20)))°")
-                .font(.system(size: 30, weight: .bold))
-                .foregroundColor(isNight ? Color.white : Color.black)
-        }
-        .padding(.all, 10)
-        .padding(.leading, 15)
-        .padding(.trailing, 15)
-        //        .overlay(RoundedRectangle(cornerRadius: 20)
-        //                    .stroke(Color.white, lineWidth: 1))
-        
-        .background(RoundedRectangle(cornerRadius: 20).fill(isNight ? Color(#colorLiteral(red: 0.3588950293, green: 0.3624484454, blue: 0.3624484454, alpha: 1)) : Color(#colorLiteral(red: 0.1277317197, green: 0.8947259689, blue: 1, alpha: 1))))
-    }
-}
+
 
 struct BackgroundView: View {
     @Binding var isNight: Bool
